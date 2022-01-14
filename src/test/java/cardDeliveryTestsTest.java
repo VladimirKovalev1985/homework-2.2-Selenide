95c055649a7d0392e328152ed4d98e4c8df65d6e
Vladimir Kovalev <boban_envorter@mail.ru>
2022-01-14 11:01:44 +0300

    add happy tests

--- src/test/java/cardDeliveryTestsTest.java
+++ src/test/java/cardDeliveryTestsTest.java
@@ -18,7 +18,7 @@ public class cardDeliveryTestsTest {
     }
 
     @Test
-    public void shouldOpen() {
+    public void shouldSendNormForm() {
 
         //открыть страницу
         open("http://localhost:9999/");
@@ -32,6 +32,246 @@ public class cardDeliveryTestsTest {
         $(".button__text").click();
         $(withText("Успешно!")).shouldHave(visible, Duration.ofSeconds(15));
         //сообщение об успешном бронировании после 15-ти секундного ожидания
+        $(".notification__content").shouldBe(visible)
+                .shouldHave(exactText("Встреча успешно забронирована на " +planningDate));
+    }
+
+    @Test
+    public void shouldSendFormWithCity(){
+        open("http://localhost:9999/");
+        String planningDate = generateDate(3);
+        $("[data-test-id=\"city\"] .input__control").setValue("Санкт-Петербург");
+        $("[data-test-id='date'] input").doubleClick().sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id=\"date\"] .input__control").setValue(planningDate);
+        $("[data-test-id=\"name\"] .input__control").setValue("Владимир Сергеев");
+        $("[data-test-id=\"phone\"] input").setValue("+79998884455");
+        $("[data-test-id=\"agreement\"] .checkbox__box").click();
+        $(".button__text").click();
+        $(withText("Успешно!")).shouldHave(visible, Duration.ofSeconds(15));
+        $(".notification__content").shouldBe(visible)
+                .shouldHave(exactText("Встреча успешно забронирована на " +planningDate));
+    }
+
+    @Test
+    public void shouldSendForm4Days(){
+        open("http://localhost:9999/");
+        String planningDate = generateDate(4);
+        $("[data-test-id=\"city\"] .input__control").setValue("Санкт-Петербург");
+        $("[data-test-id='date'] input").doubleClick().sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id=\"date\"] .input__control").setValue(planningDate);
+        $("[data-test-id=\"name\"] .input__control").setValue("Владимир Сергеев");
+        $("[data-test-id=\"phone\"] input").setValue("+79998884455");
+        $("[data-test-id=\"agreement\"] .checkbox__box").click();
+        $(".button__text").click();
+        $(withText("Успешно!")).shouldHave(visible, Duration.ofSeconds(15));
+        $(".notification__content").shouldBe(visible)
+                .shouldHave(exactText("Встреча успешно забронирована на " +planningDate));
+    }
+
+    @Test
+    public void shouldSendForm10Days(){
+        open("http://localhost:9999/");
+        String planningDate = generateDate(10);
+        $("[data-test-id=\"city\"] .input__control").setValue("Санкт-Петербург");
+        $("[data-test-id='date'] input").doubleClick().sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id=\"date\"] .input__control").setValue(planningDate);
+        $("[data-test-id=\"name\"] .input__control").setValue("Владимир Сергеев");
+        $("[data-test-id=\"phone\"] input").setValue("+79998884455");
+        $("[data-test-id=\"agreement\"] .checkbox__box").click();
+        $(".button__text").click();
+        $(withText("Успешно!")).shouldHave(visible, Duration.ofSeconds(15));
+        $(".notification__content").shouldBe(visible)
+                .shouldHave(exactText("Встреча успешно забронирована на " +planningDate));
+    }
+
+    @Test
+    public void shouldSendFormWithShortName(){
+        open("http://localhost:9999/");
+        String planningDate = generateDate(3);
+        $("[data-test-id=\"city\"] .input__control").setValue("Санкт-Петербург");
+        $("[data-test-id='date'] input").doubleClick().sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id=\"date\"] .input__control").setValue(planningDate);
+        $("[data-test-id=\"name\"] .input__control").setValue("Ян Сергеев");
+        $("[data-test-id=\"phone\"] input").setValue("+79998884455");
+        $("[data-test-id=\"agreement\"] .checkbox__box").click();
+        $(".button__text").click();
+        $(withText("Успешно!")).shouldHave(visible, Duration.ofSeconds(15));
+        $(".notification__content").shouldBe(visible)
+                .shouldHave(exactText("Встреча успешно забронирована на " +planningDate));
+    }
+
+    @Test
+    public void shouldSendFormWithShortSurname(){
+        open("http://localhost:9999/");
+        String planningDate = generateDate(3);
+        $("[data-test-id=\"city\"] .input__control").setValue("Санкт-Петербург");
+        $("[data-test-id='date'] input").doubleClick().sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id=\"date\"] .input__control").setValue(planningDate);
+        $("[data-test-id=\"name\"] .input__control").setValue("Владимир Ян");
+        $("[data-test-id=\"phone\"] input").setValue("+79998884455");
+        $("[data-test-id=\"agreement\"] .checkbox__box").click();
+        $(".button__text").click();
+        $(withText("Успешно!")).shouldHave(visible, Duration.ofSeconds(15));
+        $(".notification__content").shouldBe(visible)
+                .shouldHave(exactText("Встреча успешно забронирована на " +planningDate));
+    }
+
+    @Test
+    public void shouldSendFormWithShortNameAndSurname(){
+        open("http://localhost:9999/");
+        String planningDate = generateDate(3);
+        $("[data-test-id=\"city\"] .input__control").setValue("Санкт-Петербург");
+        $("[data-test-id='date'] input").doubleClick().sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id=\"date\"] .input__control").setValue(planningDate);
+        $("[data-test-id=\"name\"] .input__control").setValue("Ян Ян");
+        $("[data-test-id=\"phone\"] input").setValue("+79998884455");
+        $("[data-test-id=\"agreement\"] .checkbox__box").click();
+        $(".button__text").click();
+        $(withText("Успешно!")).shouldHave(visible, Duration.ofSeconds(15));
+        $(".notification__content").shouldBe(visible)
+                .shouldHave(exactText("Встреча успешно забронирована на " +planningDate));
+    }
+
+    @Test
+    public void shouldSendFormWithDoubleName(){
+        open("http://localhost:9999/");
+        String planningDate = generateDate(3);
+        $("[data-test-id=\"city\"] .input__control").setValue("Санкт-Петербург");
+        $("[data-test-id='date'] input").doubleClick().sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id=\"date\"] .input__control").setValue(planningDate);
+        $("[data-test-id=\"name\"] .input__control").setValue("Владимир-Олег Сергеев");
+        $("[data-test-id=\"phone\"] input").setValue("+79998884455");
+        $("[data-test-id=\"agreement\"] .checkbox__box").click();
+        $(".button__text").click();
+        $(withText("Успешно!")).shouldHave(visible, Duration.ofSeconds(15));
+        $(".notification__content").shouldBe(visible)
+                .shouldHave(exactText("Встреча успешно забронирована на " +planningDate));
+    }
+
+    @Test
+    public void shouldSendFormWithDoubleSurname(){
+        open("http://localhost:9999/");
+        String planningDate = generateDate(3);
+        $("[data-test-id=\"city\"] .input__control").setValue("Санкт-Петербург");
+        $("[data-test-id='date'] input").doubleClick().sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id=\"date\"] .input__control").setValue(planningDate);
+        $("[data-test-id=\"name\"] .input__control").setValue("Владимир Сергеев-Петров");
+        $("[data-test-id=\"phone\"] input").setValue("+79998884455");
+        $("[data-test-id=\"agreement\"] .checkbox__box").click();
+        $(".button__text").click();
+        $(withText("Успешно!")).shouldHave(visible, Duration.ofSeconds(15));
+        $(".notification__content").shouldBe(visible)
+                .shouldHave(exactText("Встреча успешно забронирована на " +planningDate));
+    }
+
+    @Test
+    public void shouldSendFormWithDoubleNameAndSurname(){
+        open("http://localhost:9999/");
+        String planningDate = generateDate(3);
+        $("[data-test-id=\"city\"] .input__control").setValue("Санкт-Петербург");
+        $("[data-test-id='date'] input").doubleClick().sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id=\"date\"] .input__control").setValue(planningDate);
+        $("[data-test-id=\"name\"] .input__control").setValue("Владимир-Олег Сергеев-Петров");
+        $("[data-test-id=\"phone\"] input").setValue("+79998884455");
+        $("[data-test-id=\"agreement\"] .checkbox__box").click();
+        $(".button__text").click();
+        $(withText("Успешно!")).shouldHave(visible, Duration.ofSeconds(15));
+        $(".notification__content").shouldBe(visible)
+                .shouldHave(exactText("Встреча успешно забронирована на " +planningDate));
+    }
+
+    @Test
+    public void shouldSendFormTel8(){
+        open("http://localhost:9999/");
+        String planningDate = generateDate(3);
+        $("[data-test-id=\"city\"] .input__control").setValue("Санкт-Петербург");
+        $("[data-test-id='date'] input").doubleClick().sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id=\"date\"] .input__control").setValue(planningDate);
+        $("[data-test-id=\"name\"] .input__control").setValue("Владимир Сергеев");
+        $("[data-test-id=\"phone\"] input").setValue("89998884455");
+        $("[data-test-id=\"agreement\"] .checkbox__box").click();
+        $(".button__text").click();
+        $(withText("Успешно!")).shouldHave(visible, Duration.ofSeconds(15));
+        $(".notification__content").shouldBe(visible)
+                .shouldHave(exactText("Встреча успешно забронирована на " +planningDate));
+    }
+
+    @Test
+    public void shouldSendFormTel9(){
+        open("http://localhost:9999/");
+        String planningDate = generateDate(3);
+        $("[data-test-id=\"city\"] .input__control").setValue("Санкт-Петербург");
+        $("[data-test-id='date'] input").doubleClick().sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id=\"date\"] .input__control").setValue(planningDate);
+        $("[data-test-id=\"name\"] .input__control").setValue("Владимир Сергеев");
+        $("[data-test-id=\"phone\"] input").setValue("99998884455");
+        $("[data-test-id=\"agreement\"] .checkbox__box").click();
+        $(".button__text").click();
+        $(withText("Успешно!")).shouldHave(visible, Duration.ofSeconds(15));
+        $(".notification__content").shouldBe(visible)
+                .shouldHave(exactText("Встреча успешно забронирована на " +planningDate));
+    }
+
+    @Test
+    public void shouldSendFormTelPlus0(){
+        open("http://localhost:9999/");
+        String planningDate = generateDate(3);
+        $("[data-test-id=\"city\"] .input__control").setValue("Санкт-Петербург");
+        $("[data-test-id='date'] input").doubleClick().sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id=\"date\"] .input__control").setValue(planningDate);
+        $("[data-test-id=\"name\"] .input__control").setValue("Владимир Сергеев");
+        $("[data-test-id=\"phone\"] input").setValue("+09998884455");
+        $("[data-test-id=\"agreement\"] .checkbox__box").click();
+        $(".button__text").click();
+        $(withText("Успешно!")).shouldHave(visible, Duration.ofSeconds(15));
+        $(".notification__content").shouldBe(visible)
+                .shouldHave(exactText("Встреча успешно забронирована на " +planningDate));
+    }
+
+    @Test
+    public void shouldSendFormTelWithMinus(){
+        open("http://localhost:9999/");
+        String planningDate = generateDate(3);
+        $("[data-test-id=\"city\"] .input__control").setValue("Санкт-Петербург");
+        $("[data-test-id='date'] input").doubleClick().sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id=\"date\"] .input__control").setValue(planningDate);
+        $("[data-test-id=\"name\"] .input__control").setValue("Владимир Сергеев");
+        $("[data-test-id=\"phone\"] input").setValue("+7-999-888-44-55");
+        $("[data-test-id=\"agreement\"] .checkbox__box").click();
+        $(".button__text").click();
+        $(withText("Успешно!")).shouldHave(visible, Duration.ofSeconds(15));
+        $(".notification__content").shouldBe(visible)
+                .shouldHave(exactText("Встреча успешно забронирована на " +planningDate));
+    }
+
+    @Test
+    public void shouldSendFormWithNameAndSurnameOneLetter(){
+        open("http://localhost:9999/");
+        String planningDate = generateDate(3);
+        $("[data-test-id=\"city\"] .input__control").setValue("Санкт-Петербург");
+        $("[data-test-id='date'] input").doubleClick().sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id=\"date\"] .input__control").setValue(planningDate);
+        $("[data-test-id=\"name\"] .input__control").setValue("В С");
+        $("[data-test-id=\"phone\"] input").setValue("+79998884455");
+        $("[data-test-id=\"agreement\"] .checkbox__box").click();
+        $(".button__text").click();
+        $(withText("Успешно!")).shouldHave(visible, Duration.ofSeconds(15));
+        $(".notification__content").shouldBe(visible)
+                .shouldHave(exactText("Встреча успешно забронирована на " +planningDate));
+    }
+
+    @Test
+    public void shouldSendFormWithEmptyDate(){
+        open("http://localhost:9999/");
+        String planningDate = generateDate(3);
+        $("[data-test-id=\"city\"] .input__control").setValue("Санкт-Петербург");
+       // $("[data-test-id='date'] input").doubleClick().sendKeys(Keys.BACK_SPACE);
+        // $("[data-test-id=\"date\"] .input__control").setValue(planningDate);
+        $("[data-test-id=\"name\"] .input__control").setValue("Владимир Сергеев");
+        $("[data-test-id=\"phone\"] input").setValue("+79998884455");
+        $("[data-test-id=\"agreement\"] .checkbox__box").click();
+        $(".button__text").click();
+        $(withText("Успешно!")).shouldHave(visible, Duration.ofSeconds(15));
         $(".notification__content").shouldBe(visible)
                 .shouldHave(exactText("Встреча успешно забронирована на " +planningDate));
     }
